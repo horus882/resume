5078deb290ecdca60446d5592f5a1bb46c519e48
ml-brent <brent.mc@medialand.tw>
2021-03-19 18:22:38 +0800

    增加日期

--- src/components/about/Experience.vue
+++ src/components/about/Experience.vue
@@ -5,7 +5,7 @@
       <li>
         <h4>Moulin-Orange 橘子磨坊數位創意溝通股份有限公司</h4>
         <p class="label">資深前端工程師</p>
-        <p class="period">2018.10 － 2021.03</p>
+        <p class="period">2018.10 － 2021.03.17</p>
         <p class="label">前端工程師</p>
         <p class="period">2014.01 － 2018.10</p>
       </li>
